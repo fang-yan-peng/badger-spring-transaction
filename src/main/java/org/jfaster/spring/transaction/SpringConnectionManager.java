ec7d08d54856fc86d2fdb4dcef85e7710c197d01
yanpengfang <yanpengfang@tencent.com>
2019-02-12 23:19:57 +0800

    增加spring事务

---
+++ src/main/java/org/jfaster/spring/transaction/SpringConnectionManager.java
@@ -0,0 +1,23 @@
+package org.jfaster.spring.transaction;
+
+import java.sql.Connection;
+
+import javax.sql.DataSource;
+
+import org.jfaster.badger.spi.SpiMeta;
+import org.jfaster.badger.transaction.ConnectionManager;
+import org.springframework.jdbc.datasource.DataSourceUtils;
+
+@SpiMeta(name = "spring")
+public class SpringConnectionManager implements ConnectionManager {
+
+    public Connection getConnection(DataSource dataSource) {
+        return DataSourceUtils.getConnection(dataSource);
+    }
+
+
+    public void releaseConnection(Connection conn, DataSource dataSource) {
+        DataSourceUtils.releaseConnection(conn, dataSource);
+    }
+
+}
